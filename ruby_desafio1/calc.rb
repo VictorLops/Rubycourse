0146537c8f4cbf716d29b77c1ad78e0841d7dad3
Victor lopes <victorlopesgtx@gmail.com>
2023-01-23 07:53:55 -0300

    calc.rb

---
+++ ruby_desafio1/calc.rb
@@ -0,0 +1,9 @@
+print 'Digite o primeiro número inteiro: '
+# .to_i trasforma a string em um numero inteiro
+number1 = gets.chomp.to_i
+print 'Digite o segundo numero inteiro: '
+number2 = gets.chomp.to_i
+puts "O resultado da adição entre dois numeros é #{number1 + number2}"
+puts "O resultado da adição entre dois numeros é #{number1 - number2}"
+puts "O resultado da adição entre dois numeros é #{number1 * number2}"
+puts "O resultado da adição entre dois numeros é #{number1 / number2}"
