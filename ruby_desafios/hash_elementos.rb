a69d22e31b065535237e3e8b18c828810007a8a4
Victor lopes <victorlopesgtx@gmail.com>
2023-02-08 17:57:09 -0300

    hash_elementos.rb

---
+++ ruby_desafios/hash_elementos.rb
@@ -0,0 +1,48 @@
+
+resultado = ''
+
+loop do
+
+  puts resultado
+
+  puts "\n seja bem-vindos ao tutorial"
+
+  puts "\n escolha sua magia logo a baixo"
+
+
+  puts '1 - água'
+  puts '2 - fogo'
+  puts '3 - terra'
+  puts '4 - ar'
+  puts '5 - desconectar'
+  print 'Opção: '
+
+  option = gets.chomp.to_i
+
+  if option == 1
+    água = {'bolhas' => 'perfurantes'}
+    água.each do |keys, value|
+      resultado = "sua habilidade é #{keys} #{value}"
+    end
+  elsif option == 2
+    fogo = {'labareta' => 'explosiva'}
+    fogo.each do |keys, value|
+      resultado = "sua habilidade é #{keys} #{value}"
+    end
+  elsif option == 3
+    terra = {'pedras' => 'pontiagudas'}
+    terra.each do |keys, value|
+      resultado = "sua habilidade é #{keys} #{value}"
+    end
+  elsif option == 4
+    ar = {'corte' => 'rapidos'}
+    ar.each do |keys, value|
+      resultado = "sua habilidade é #{keys} #{value}"
+    end
+  elsif option == 5
+    break
+  else
+    resultado = 'Opção invalida'
+  end
+  system 'clear'
+end
